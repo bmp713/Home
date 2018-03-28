99c9d945f4b19fdf1a1158960c5b2ef325089aae
Brandon Michael Piper <bmp713@yahoo.com>
2018-03-27 23:36:50 -0700

    Add files via upload

--- style.js
+++ style.js
@@ -1,43 +1,42 @@
 
 window.addEventListener("load", fixNavbar);
-window.addEventListener("resize", fixNavbar);
 window.addEventListener("scroll", fixNavbar);
 
 function fixNavbar(){
     // Desktop view
-    if ( window.matchMedia("(min-width: 768px)" ).matches ){
-        console.log('GREATER than 768px');
-        document.getElementById('name').style.display = "none";
-        document.getElementById('title').style.font = "36px sans-serif";
-        document.getElementById('resume_mobile').style.display = "none";
+    /*
+    console.log(window.pageYOffset, document.body.clientHeight, document.body.scrollHeight);
 
+    if(document.body.scrollHeight >= document.body.clientHeight){
+        console.log("window.pageYOffset >= document.height");
+        document.getElementById('copyright').style.height = '150px';
+    }
+    else{
+        console.log("ELSE");
+        document.getElementById('copyright').style.height = '50px';
+    }
+    */
+    if ( window.matchMedia("(min-width: 768px)" ).matches ){
+        console.log('DESKTOP');
         if( window.pageYOffset >= 150 ){
-            document.getElementById("header").style.display = 'none';
+            //document.getElementById("content_1").style.top = '50';
             document.getElementById("navbar").style.position = 'fixed';
             document.getElementById("navbar").style.top = '0';
-            //document.getElementById("content").style.top = '150';
         }
         else{
-            document.getElementById("header").style.display = 'block';
+            //document.getElementById("content_1").style.top = '0';
             document.getElementById("navbar").style.position = 'relative';
             document.getElementById("navbar").style.top = '150';
-            //document.getElementById("content").style.top = '0';
         }
     }
     // Mobile View
     else{
-        console.log('LESS than 768px');
-        document.getElementById('name').style.display = "block";
-        document.getElementById('title').style.font = "16px sans-serif";
-        document.getElementById('resume_mobile').style.display = "block";
-
-        document.getElementById("header").style.display = 'none';
+        console.log('MOBILE');
         document.getElementById("navbar").style.position = 'fixed';
         document.getElementById("navbar").style.top = '0';
+        //document.getElementById('copyright_2').appendChild( document.createElement('br') );
     }
 }
-
-
 function smoothScroll( elementId ){
     var offset = 35; // Might need to make responsive
     var current = window.pageYOffset;
